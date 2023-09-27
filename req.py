84608eedcfe86fc224dc8fe9738d4549a52658f4
akhil raj <akhil.raj@blenheimchalcot.com>
2023-09-27 12:03:55 +0530

    modified for better pylint rating

--- req.py
+++ req.py
@@ -1,17 +1,19 @@
+"""program to demonstrate requests"""
 import requests
-r_for_content=requests.get('https://httpbin.org/basic-auth/something/prx',auth=('something','prx'))
-r_for_image=requests.get('https://picsum.photos/200/300')
+r_for_content=requests.get('https://httpbin.org/basic-auth/something/prx',
+auth=('something','prx'),timeout=10)
+r_for_image=requests.get('https://picsum.photos/200/300',timeout=10)
 #print(r_for_content.text) for site text
 #print(r_for_content.status_code) for status_code ex:200 for active
 # print(r_for_content.ok) prints true if status_code<400
-# print(r_for_content.headers) prints some information 
+# print(r_for_content.headers) prints some information
 with open('random_image.png', 'wb') as f:
     f.write(r_for_image.content)
 print(r_for_content.text)
-payload={'username':'something', 'password':'prx'} 
-r_post=requests.post("http://httpbin.org/post",data=payload)
+payload={'username':'something', 'password':'prx'}
+r_post=requests.post("http://httpbin.org/post",data=payload,timeout=10)
 #print(r_post.text)
 #or
 r_dict=r_post.json()
 print(r_dict['form'])
-#print(r_for_content.url) to print the url 
+#print(r_for_content.url) to print the url
